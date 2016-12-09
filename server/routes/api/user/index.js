0b976e8ce4590dd5b63400217bac6087606c3a42
bmmanson <bmmanson@gmail.com>
2016-12-09 16:26:36 +0200

    webpack, react installed

--- server/routes/api/user/index.js
+++ server/routes/api/user/index.js
@@ -3,7 +3,7 @@ var Promise = require('bluebird');
 var router = express.Router();
 
 router.get('/', function (req, res, next) {
-
+	
 });
 
 module.exports = router;
--- server/routes/index.js
+++ server/routes/index.js
@@ -1,10 +1,6 @@
 var express = require('express');
 var router = express.Router();
 
-// router.get('/', function (req, res, next) {
-// 	res.send("ROOT ROUTE");
-// });
-
 router.use('/api', require('./api/'));
 
 module.exports = router;
